ec2d95717a6e565c4331d242275f69f4e2075138
Ian <ianabildskou@gmail.com>
2018-11-02 21:03:34 -0700

    We have sound!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Tone from 'tone';
 
 class App extends Component {
   render() {
+    //create a synth and connect it to the master output (your speakers)
+    var synth = new Tone.Synth().toMaster()
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease('C4', '8n')
     return (
       <div className="App">
         <h1>gyroSynth</h1>
