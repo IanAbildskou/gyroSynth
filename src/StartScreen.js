3922bdf3a39d81b5a5173955d925d9ff39509916
Ian <ianabildskou@gmail.com>
2019-10-29 18:03:35 +0100

    added arm animation to start screen

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -46,6 +46,7 @@ class StartScreen extends Component {
                 ? <div>
                     <p>Hold your phone like a drumstick and hit the empty air in front of you to produce sound.</p>
                     <p>Rotate around yourself to change pitch.</p>
+                    <img alt='' className='arm-svg' src="assets/arm.svg"/>
                     <div className='start-button main-button' onClick={close}>Let's go!</div>
                   </div>
                 : <div>
